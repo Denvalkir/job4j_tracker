2da4e196de72f8255829b8cebccaf882ceeb5838
Denvalkir <denbodrov@yandex.ru>
2021-07-18 02:36:56 +0500

    1.8. Состояние объекта [#167388 #178740]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -3,7 +3,20 @@ package ru.job4j.oop;
 public class Battery {
     private int load;
 
+    public Battery(int size) {
+        this.load = size;
+    }
+
     public void exchange(Battery another) {
-        this.load = this.load + another.load;
+        another.load = this.load + another.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery bat1 = new Battery(10);
+        Battery bat2 = new Battery(5);
+        bat1.exchange(bat2);
+        System.out.println(bat1.load);
+        System.out.println(bat2.load);
     }
 }
