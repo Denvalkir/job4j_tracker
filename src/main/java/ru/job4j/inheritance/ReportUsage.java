f7e37c27ae58f7885c8e8e87acc190eab8e87830
Denvalkir <denbodrov@yandex.ru>
2021-07-19 20:49:57 +0500

    4. Переопределение [#174890]

---
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class ReportUsage {
+    public static void main(String[] args) {
+        JSONReport reports = new JSONReport();
+        String texts = reports.generate("Report's name", "Report's body");
+        System.out.println(texts);
+    }
+}
