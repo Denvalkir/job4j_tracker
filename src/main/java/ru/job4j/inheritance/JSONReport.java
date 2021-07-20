a26dce405305cedf6cc7a3ee673211622d57c705
Denvalkir <denbodrov@yandex.ru>
2021-07-20 13:30:55 +0500

    4. Переопределение [#174890 #179416]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -4,6 +4,7 @@ public class JSONReport extends TextReport {
 
     @Override
     public String generate(String name, String body) {
-        return "\"" + name + "\"" + " : " + "name,\n" + "\"" + body + "\"" + " : " + "body";
+        String s = System.lineSeparator();
+        return "{" + s + "\"" + name + "\" : name," + s + "\"" + body + "\" : body" + s + "}";
     }
 }
