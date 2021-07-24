474d133a732a5ebb0e8f5e1dfd902fbeee073898
Denvalkir <denbodrov@yandex.ru>
2021-07-24 19:47:00 +0500

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,11 +4,12 @@ import java.time.LocalDateTime;
 
 public class Item {
     private int id;
+
     private String name;
+
     private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
-
     }
 
     public Item(String name) {
@@ -18,10 +19,6 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
     }
 
     public int getId() {
@@ -38,5 +35,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -11,10 +13,34 @@ public class Tracker {
         return item;
     }
 
+    public Item[] findAll() {
+        int sizeTempItems = 0;
+        Item[] tempItems = new Item[size];
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                tempItems[sizeTempItems] = items[i];
+                sizeTempItems++;
+            }
+        }
+        return Arrays.copyOf(tempItems, sizeTempItems);
+    }
+
+    public Item[] findByName(String key) {
+        int sizeTempItems = 0;
+        Item[] tempItems = new Item[size];
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                tempItems[sizeTempItems] = items[i];
+                sizeTempItems++;
+            }
+        }
+        return Arrays.copyOf(tempItems, sizeTempItems);
+    }
+
     public Item findById(int id) {
         Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
             if (item.getId() == id) {
                 rsl = item;
                 break;
