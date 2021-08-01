8045550abff7dcc7b9beec0462bfb44b90128c34
Denvalkir <denbodrov@yandex.ru>
2021-08-01 22:19:42 +0500

    3. Интерфейс Input [#181092]

---
+++ src/main/java/poly/Bus.java
@@ -0,0 +1,18 @@
+package poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public int countPassengers() {
+        return 0;
+    }
+
+    @Override
+    public int countFuel(double price) {
+        return 0;
+    }
+}
---
+++ src/main/java/poly/Transport.java
@@ -0,0 +1,9 @@
+package poly;
+
+public interface Transport {
+    void drive();
+
+    int countPassengers();
+
+    int countFuel(double price);
+}
