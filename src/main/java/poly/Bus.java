70af28e901366887a4bc057a9357a8e3949dcec0
Denvalkir <denbodrov@yandex.ru>
2021-08-03 21:31:19 +0500

    3. Интерфейс Input [#181092 #184499]

--- src/main/java/poly/Bus.java
+++ src/main/java/poly/Bus.java
@@ -4,16 +4,16 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
-
+        System.out.println("Автобус движется по трассе");
     }
 
     @Override
     public void passengers(int countPassengers) {
-
+        System.out.println("Количество пасажиров - " + countPassengers);
     }
 
     @Override
     public double price(double countRefuel) {
-        return 0;
+        return countRefuel / 10;     // например, вычисляем цену билета :)
     }
 }
